740bf00dad876b85bd26ffea848aa6737456174d
LeeYeJin <dldpwls0626@gmail.com>
2024-07-29 00:23:24 +0900

    올리브영 크롤링

--- 크롤링/Oliveyoung.py
+++ 크롤링/Oliveyoung.py
@@ -28,17 +28,67 @@ Lotion_dataset
 all_reviews = []
 
 #상품페이지 
-for page_num in range(1,5):
-    olive_url = f"https://www.oliveyoung.co.kr/store/display/getMCategoryList.do?dispCatNo=100000100010016&fltDispCatNo=&prdSort=01&pageIdx={page_num}&rowsPerPage=48&searchTypeSort=btn_thumb&plusButtonFlag=N&isLoginCnt=0&aShowCnt=0&bShowCnt=0&cShowCnt=0&trackingCd=Cat100000100010016_Small&amplitudePageGubun=&t_page=&t_click=&midCategory=%EB%A1%9C%EC%85%98&smallCategory=%EC%A0%84%EC%B2%B4&checkBrnds=&lastChkBrnd="
+for page_num in range(1,6):
+    olive_url = f"https://www.oliveyoung.co.kr/store/display/getMCategoryList.do?dispCatNo=100000100010016&fltDispCatNo=&prdSort=01&pageIdx={page_num}&rowsPerPage=48&searchTypeSort=btn_thumb&plusButtonFlag=N&isLoginCnt=1&aShowCnt=0&bShowCnt=0&cShowCnt=0&trackingCd=Cat100000100010016_Small&amplitudePageGubun=&t_page=&t_click=&midCategory=%EB%A1%9C%EC%85%98&smallCategory=%EC%A0%84%EC%B2%B4&checkBrnds=&lastChkBrnd="
     driver.get(olive_url)
     time.sleep(1)
 
-
+    if page_num == 5:
+        for i in range(2, 13):
+            for j in range(1, 3):
+                driver.find_element(By.XPATH, f'//*[@id="Contents"]/ul[{i}]/li[{j}]/div/div/a').click()
+                time.sleep(1) 
+                brand = driver.find_element(By.CLASS_NAME, "prd_brand").text
+                name = driver.find_element(By.CLASS_NAME, "prd_name").text
+                price = driver.find_element(By.CLASS_NAME, "price-1").text
+                sale_price = driver.find_element(By.CLASS_NAME, "price-2").text
+                picture = driver.find_element(By.XPATH, '//*[@id="mainImg"]').get_attribute('src')
+                url = driver.find_element(By.XPATH, '/html/head/meta[8]').get_attribute('content')
+                
+                driver.find_element(By.CSS_SELECTOR, "#buyInfo > a").click()
+                time.sleep(1)
+                volume = driver.find_element(By.XPATH,'//*[@id="artcInfo"]/dl[2]/dd').text
+                skin_type = driver.find_element(By.XPATH,'//*[@id="artcInfo"]/dl[3]/dd').text
+                ingredient = driver.find_element(By.XPATH,'//*[@id="artcInfo"]/dl[8]/dd').text
+                
+                print(brand)
+                print(name)
+                print(price) 
+                print(sale_price)
+                print(picture)
+                print(url)
+                print(volume)
+                print(skin_type)
+                print(ingredient)
+                
+                #리뷰 크롤링
+                driver.find_element(By.CSS_SELECTOR, "#reviewInfo > a").click() #리뷰버튼 클릭
+                time.sleep(2) 
+                driver.find_element(By.XPATH, '//*[@id="gdasSort"]/li[3]/a').click() #최신순 클릭
+                time.sleep(1) 
+                driver.find_element(By.CSS_SELECTOR, "#searchType_3").click() #체험단 체크 해제 
+                time.sleep(1)
+                
+                k = 1
+                while True:
+                    reviews = [] #수정
+                    for r in range(1,11):
+                        review = driver.find_element(By.XPATH, f'//*[@id="gdasList"]/li[{r}]/div[2]/div[3]').text 
+                        reviews.append([review])
+                    all_reviews.extend(reviews)
+                    if k < 10:
+                        button = driver.find_element(By.XPATH, f'//*[@id="gdasContentsArea"]/div/div[8]/a[{k}]').click() 
+                        k += 1             
+                        time.sleep(2)
+                    else:
+                        print(all_reviews)
+                        break
+                
+            driver.back()
     for i in range(2,14):
         for j in range(1, 5):
             driver.find_element(By.XPATH, f'//*[@id="Contents"]/ul[{i}]/li[{j}]/div/div/a').click()
-            
-            driver.find_element(By.CLASS_NAME, 'prd_info').click()
+            # driver.find_element(By.CLASS_NAME, 'prd_info').click()
             time.sleep(1) 
             brand = driver.find_element(By.CLASS_NAME, "prd_brand").text
             name = driver.find_element(By.CLASS_NAME, "prd_name").text
@@ -70,24 +120,22 @@ for page_num in range(1,5):
             driver.find_element(By.XPATH, '//*[@id="gdasSort"]/li[3]/a').click() #최신순 클릭
             time.sleep(1) 
             driver.find_element(By.CSS_SELECTOR, "#searchType_3").click() #체험단 체크 해제 
-            
+            time.sleep(1)
             
             k = 1
             while True:
                 reviews = [] #수정
-                review_box = driver.find_element(By.CLASS_NAME,'review_cont').text
-                time.sleep(1)
-                for review in review_box:
-                    review = driver.find_element(By.CLASS_NAME, 'txt_inner').text 
+                for r in range(1,11):
+                    review = driver.find_element(By.XPATH, f'//*[@id="gdasList"]/li[{r}]/div[2]/div[3]').text 
                     reviews.append([review])
                 all_reviews.extend(reviews)
                 if k < 10:
-                    button = driver.find_element(By.XPATH, f'//*[@id="gdasContentsArea"]/div/div[7]/a[{k}]').click() 
+                    button = driver.find_element(By.XPATH, f'//*[@id="gdasContentsArea"]/div/div[8]/a[{k}]').click() 
                     k += 1             
                     time.sleep(2)
                 else:
                     print(all_reviews)
                     break
-                
-            # driver.back()
+            Lotion_dataset.loc[len(Lotion_dataset)] = [brand, name,price,sale_price,picture,url,volume,skin_type,ingredient, all_reviews]    
+            driver.back()
             
